9b77557b4b187df74e2e35fa695879a8b6520faa
Mohasina <45617605+moonja@users.noreply.github.com>
2022-05-14 15:14:23 +0600

    Add files via upload

---
+++ Online-Examination-System/Online-Examination-System-main/README.md
@@ -0,0 +1,2 @@
+# Online-Examination-System
+In  this system a educational institute can take exam online. I can use html5,css3,javascripte,php and mySql database to develop it.
---
+++ Online-Examination-System/Online-Examination-System-main/online-examination/Front_about.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>About- Online Exam</title>
+<link  rel="stylesheet" href="css/bootstrap.min.css"/>
+ <link  rel="stylesheet" href="css/bootstrap-theme.min.css"/>    
+ <link rel="stylesheet" href="css/main.css">
+ <link  rel="stylesheet" href="css/font.css">
+ <script src="js/jquery.js" type="text/javascript"></script>
+
+  <script src="js/bootstrap.min.js"  type="text/javascript"></script>
+ 	<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+	<!--alert message-->
+<?php if(@$_GET['w'])
+{echo'<script>alert("'.@$_GET['w'].'");</script>';}
+?>
+<!--alert message end-->
+
+</head>
+
+<body>
+
+<!--header start-->
+<div class="row header">
+<div class="col-lg-6">
+<span class="logo">Online Examination</span></div>
+<div class="col-md-2">
+</div>
+<div class="col-md-4">
+<?php
+ include_once 'dbConnection.php';
+session_start();
+  if((!isset($_SESSION['email']))){
+echo '<a href="#" class="pull-right sub1 btn title3" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-log-in" aria-hidden="true"></span>&nbsp;Signin</a>&nbsp;';}
+else
+{
+echo '<a href="logout.php?q=about.php" class="pull-right sub1 btn title3"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>&nbsp;Signout</a>&nbsp;';}
+?>
+
+<a href="index.php" class="pull-right btn sub1 title3"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>&nbsp;Home</a>&nbsp;
+</div></div>
+
+<!--sign in modal start-->
+<div class="modal fade" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content title1">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title title1"><span style="color:orange">Log In</span></h4>
+      </div>
+      <div class="modal-body">
+        <form class="form-horizontal" action="login.php?q=index.php" method="POST">
+<fieldset>
+
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-3 control-label" for="email"></label>  
+  <div class="col-md-6">
+  <input id="email" name="email" placeholder="Enter your email-id" class="form-control input-md" type="email">
+    
+  </div>
+</div>
+
+<!-- Password input-->
+<div class="form-group">
+  <label class="col-md-3 control-label" for="password"></label>
+  <div class="col-md-6">
+    <input id="password" name="password" placeholder="Enter your Password" class="form-control input-md" type="password">
+    
+  </div>
+</div>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Log in</button>
+		</fieldset>
+</form>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!--sign in modal closed-->
+
+<!--header end-->
+
+<div class="bg1">
+<div class="row">
+<div class="col-md-3"></div>
+<div class="col-md-6 panel" style="background-image:url(image/bg1.jpg); min-height:430px;">
+<h2 align="center" style="font-family:'typo'; color:#000066">Online Examination</h2>
+<div style="font-size:14px">
+
+On-line examinations contents providers to focus on creating effective assessment questions and focusing on exam’s feedback delivery to students. In the paper we present techniques that are pertinent to the elements of assessment process: answers submission, computerized grading, and feedback after submission.<br><br>As the modern organizations are automated and computers are working as per the instructions, it becomes essential for the coordination of human beings, commodity and computers in a modern organization. <br><br> The administrators ,instructor,Students who are attending for online examination can communicate with the system through this projects, thus facilitating effective implementation and monitoring of various activities of Online Examinations like conducting Exams as per scheduled basis and delivering result to that particular use or student.And the details of students who attempted Online Examination are maintained at administrator.';
+
+
+<div class="form-group" align="center">
+<a href="index.php" ><span>Back</span></a>
+</div>
+</div><!--col-md-6 end-->
+<div class="col-md-3"></div></div>
+</div></div>
+</div><!--container end-->
+
+
+<!--Footer start-->
+<div class="row footer">
+<div class="col-md-4 box">
+<a>Online Examination System</a>
+</div>
+
+<div class="col-md-4 box">
+<a >Bangladesh University of business & Technology</a></div>
+<div class="col-md-3 box">
+<a >©Mohasina Jannat Moon</a></div></div>
+<!--Modal for admin login-->
+	 <div class="modal fade" id="login">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title"><span style="color:orange;font-family:'typo' ">LOGIN</span></h4>
+      </div>
+      <div class="modal-body title1">
+<div class="row">
+<div class="col-md-3"></div>
+<div class="col-md-6">
+<form role="form" method="post" action="admin.php?q=index.php">
+<div class="form-group">
+<input type="text" name="uname" maxlength="20"  placeholder="Admin user id" class="form-control"/> 
+</div>
+<div class="form-group">
+<input type="password" name="password" maxlength="15" placeholder="Password" class="form-control"/>
+</div>
+<div class="form-group" align="center">
+<input type="submit" name="login" value="Login" class="btn btn-primary" />
+</div>
+</form>
+</div><div class="col-md-3"></div></div>
+      </div>
+      <!--<div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>-->
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!--footer end-->
+
+
+</body>
+</html>
